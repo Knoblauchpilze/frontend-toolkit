fd539d81e3becf1f4900a56894192b093638fcaa
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-12-18 19:18:39 +0100

    feat: Base Svelte components added

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export { default as StyledActionButton } from '$lib/components/core/StyledActionButton.svelte';
+export { default as StyledTitle } from '$lib/components/core/StyledTitle.svelte';
+export { default as StyledLink } from '$lib/components/core/StyledLink.svelte';
+
+export { default as FlexContainer } from '$lib/components/layout/FlexContainer.svelte';
+export { default as HeroContainer } from '$lib/components/layout/HeroContainer.svelte';

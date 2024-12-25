e4d1c7875088349f2083a444e3229815a8bbb315
KnoblauchPilze <prolific-programmer@tuta.com>
2024-12-25 11:15:37 +0100

    fix: Always set the input with a text-primary color

--- src/lib/components/FormField.svelte
+++ src/lib/components/FormField.svelte
@@ -6,13 +6,16 @@
 	interface Props {
 		label?: string;
 		labelId: string;
+		styling?: string;
 		children?: Snippet;
 	}
 
-	let { label = 'My label', labelId, children }: Props = $props();
+	let { label = 'My label', labelId, styling = 'text-primary', children }: Props = $props();
 </script>
 
-<FlexContainer extensible={false} align={'stretch'} styling={'text-secondary'}>
+<FlexContainer extensible={false} align={'stretch'} {styling}>
 	<label for={labelId}>{label}</label>
-	{@render children?.()}
+	<div class="text-primary">
+		{@render children?.()}
+	</div>
 </FlexContainer>

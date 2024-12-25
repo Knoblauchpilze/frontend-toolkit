65473545b18cf2893078b0f1617115da20315b8c
KnoblauchPilze <prolific-programmer@tuta.com>
2024-12-25 10:46:51 +0100

    feat: Added more base components

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+
+	import { FlexContainer } from '$lib/index.js';
+
+	interface Props {
+		height?: string;
+		children?: Snippet;
+	}
+
+	let { height = 'h-1/10', children }: Props = $props();
+</script>
+
+<FlexContainer
+	vertical={false}
+	bgColor={'bg-black'}
+	styling="fixed bottom-0 w-full {height} text-white"
+>
+	{@render children?.()}
+</FlexContainer>

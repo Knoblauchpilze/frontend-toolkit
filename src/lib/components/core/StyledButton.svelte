7f377a80acf09a1f5a8bf320925703370ab82ed0
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-12-25 10:46:51 +0100

    feat: Added more base components

---
+++ src/lib/components/core/StyledButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface Props {
+		text?: string;
+		enabled?: boolean;
+	}
+
+	let { text = 'Click me', enabled = true }: Props = $props();
+</script>
+
+{#if enabled}
+	<button class="rounded-[8px] bg-primary px-12 py-4 text-secondary hover:bg-primary-hover"
+		>{text}</button
+	>
+{:else}
+	<!-- https://stackoverflow.com/questions/34684467/dont-redirect-on-button-click-with-css -->
+	<button disabled class="rounded-[8px] bg-primary-selected px-12 py-4 text-secondary"
+		>{text}</button
+	>
+{/if}
---
+++ src/lib/components/layout/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { type Snippet } from 'svelte';
+
+	import { FlexContainer } from '$lib/index.js';
+
+	interface Props {
+		children?: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
+
+<FlexContainer
+	vertical={false}
+	bgColor={'bg-black'}
+	styling={'w-full h-1/10 fixed top-0 left-0 text-white'}
+>
+	{@render children?.()}
+</FlexContainer>

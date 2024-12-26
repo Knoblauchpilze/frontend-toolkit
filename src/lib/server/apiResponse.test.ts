59826b6070c6d4b95e2520410651a784d2153820
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2024-12-26 16:00:14 +0100

    feat: Added handling for various HTTP status code and tests

--- src/lib/server/apiResponse.test.ts
+++ src/lib/server/apiResponse.test.ts
@@ -5,36 +5,51 @@ import {
 	ApiResponse,
 	Status
 } from './apiResponse.js';
+import { HttpStatus } from './httpStatusCode.js';
 
 const SAMPLE_REQUEST_ID = 'e0bcdaa7-9e60-411b-a308-75e0c079abed';
+const SAMPLE_RESPONSE_DETAILS = {
+	requestId: SAMPLE_REQUEST_ID,
+	status: 'unknown-status',
+	details: {}
+};
 
 describe('Creating an ApiResponse', () => {
 	it('should use the provided request identifier', () => {
-		const actual = new ApiResponse({
-			requestId: SAMPLE_REQUEST_ID,
-			status: 'SUCCESS',
-			details: {}
-		});
+		const actual = new ApiResponse(
+			{
+				requestId: SAMPLE_REQUEST_ID,
+				status: 'SUCCESS',
+				details: {}
+			},
+			HttpStatus.OK
+		);
 
 		expect(actual.getRequestId()).toBe(SAMPLE_REQUEST_ID);
 	});
 
 	it('should not expect request identifier to be a uuid', () => {
-		const actual = new ApiResponse({
-			requestId: 'my-custom-identifier',
-			status: 'SUCCESS',
-			details: {}
-		});
+		const actual = new ApiResponse(
+			{
+				requestId: 'my-custom-identifier',
+				status: 'SUCCESS',
+				details: {}
+			},
+			HttpStatus.OK
+		);
 
 		expect(actual.getRequestId()).toBe('my-custom-identifier');
 	});
 
 	it('should recognize SUCCESS status', () => {
-		const actual = new ApiResponse({
-			requestId: SAMPLE_REQUEST_ID,
-			status: 'SUCCESS',
-			details: {}
-		});
+		const actual = new ApiResponse(
+			{
+				requestId: SAMPLE_REQUEST_ID,
+				status: 'SUCCESS',
+				details: {}
+			},
+			HttpStatus.OK
+		);
 
 		expect(actual.getStatus()).toBe(Status.SUCCESS);
 		expect(actual.isSuccess()).toBe(true);
@@ -42,11 +57,14 @@ describe('Creating an ApiResponse', () => {
 	});
 
 	it('should recognize ERROR status', () => {
-		const actual = new ApiResponse({
-			requestId: SAMPLE_REQUEST_ID,
-			status: 'ERROR',
-			details: {}
-		});
+		const actual = new ApiResponse(
+			{
+				requestId: SAMPLE_REQUEST_ID,
+				status: 'ERROR',
+				details: {}
+			},
+			HttpStatus.BAD_REQUEST
+		);
 
 		expect(actual.getStatus()).toBe(Status.ERROR);
 		expect(actual.isSuccess()).toBe(false);
@@ -54,21 +72,55 @@ describe('Creating an ApiResponse', () => {
 	});
 
 	it('should recognize unknown status as ERROR', () => {
-		const actual = new ApiResponse({
-			requestId: SAMPLE_REQUEST_ID,
-			status: 'unknown-status',
-			details: {}
-		});
+		const actual = new ApiResponse(
+			{
+				requestId: SAMPLE_REQUEST_ID,
+				status: 'unknown-status',
+				details: {}
+			},
+			HttpStatus.BAD_REQUEST
+		);
 
 		expect(actual.getStatus()).toBe(Status.ERROR);
 		expect(actual.isSuccess()).toBe(false);
 		expect(actual.isError()).toBe(true);
 	});
+
+	it('should correctly interpret 2xx HTTP status code', () => {
+		let actual = new ApiResponse(SAMPLE_RESPONSE_DETAILS, HttpStatus.OK);
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(false);
+
+		actual = new ApiResponse(SAMPLE_RESPONSE_DETAILS, HttpStatus.NO_CONTENT);
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(false);
+	});
+
+	it('should correctly interpret 4xx HTTP status code', () => {
+		let actual = new ApiResponse(SAMPLE_RESPONSE_DETAILS, HttpStatus.BAD_REQUEST);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(true);
+		expect(actual.is5xxError()).toBe(false);
+
+		actual = new ApiResponse(SAMPLE_RESPONSE_DETAILS, HttpStatus.FORBIDDEN);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(true);
+		expect(actual.is5xxError()).toBe(false);
+	});
+
+	it('should correctly interpret 5xx HTTP status code', () => {
+		const actual = new ApiResponse(SAMPLE_RESPONSE_DETAILS, HttpStatus.INTERNAL_SERVER_ERROR);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(true);
+	});
 });
 
 describe('Creating a failed ApiResponse', () => {
 	it('should have ERROR status', () => {
-		const actual = createFailedApiResponse({});
+		const actual = createFailedApiResponse({}, HttpStatus.INTERNAL_SERVER_ERROR);
 
 		expect(actual.isError()).toBe(true);
 	});
@@ -81,22 +133,84 @@ describe('Creating a failed ApiResponse', () => {
 				anotherKey: [2.3, 3.7]
 			}
 		};
-		const actual = createFailedApiResponse(expectedDetails);
+		const actual = createFailedApiResponse(expectedDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 
 		expect(actual.getDetails()).toBe(expectedDetails);
+	});
+
+	it('should correctly interpret 2xx HTTP status code', () => {
+		let actual = createFailedApiResponse({}, HttpStatus.OK);
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(false);
+
+		actual = createFailedApiResponse({}, HttpStatus.NO_CONTENT);
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(false);
+	});
+
+	it('should correctly interpret 4xx HTTP status code', () => {
+		let actual = createFailedApiResponse({}, HttpStatus.BAD_REQUEST);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(true);
+		expect(actual.is5xxError()).toBe(false);
+
+		actual = createFailedApiResponse({}, HttpStatus.FORBIDDEN);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(true);
+		expect(actual.is5xxError()).toBe(false);
+	});
+
+	it('should correctly interpret 5xx HTTP status code', () => {
+		const actual = createFailedApiResponse({}, HttpStatus.INTERNAL_SERVER_ERROR);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(true);
 	});
 });
 
 describe('Creating an empty success ApiResponse', () => {
 	it('should have SUCCESS status', () => {
-		const actual = createEmptySuccessApiResponse();
+		const actual = createEmptySuccessApiResponse(HttpStatus.NO_CONTENT);
 
 		expect(actual.isSuccess()).toBe(true);
 	});
 
 	it('should not have any details', () => {
-		const actual = createEmptySuccessApiResponse();
+		const actual = createEmptySuccessApiResponse(HttpStatus.NO_CONTENT);
 
 		expect(actual.getDetails()).toEqual({});
+	});
+
+	it('should correctly interpret 2xx HTTP status code', () => {
+		let actual = createEmptySuccessApiResponse(HttpStatus.OK);
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(false);
+
+		actual = createEmptySuccessApiResponse(HttpStatus.NO_CONTENT);
+		expect(actual.is2xxOk()).toBe(true);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(false);
+	});
+
+	it('should correctly interpret 4xx HTTP status code', () => {
+		let actual = createEmptySuccessApiResponse(HttpStatus.BAD_REQUEST);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(true);
+		expect(actual.is5xxError()).toBe(false);
+
+		actual = createEmptySuccessApiResponse(HttpStatus.FORBIDDEN);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(true);
+		expect(actual.is5xxError()).toBe(false);
+	});
+
+	it('should correctly interpret 5xx HTTP status code', () => {
+		const actual = createEmptySuccessApiResponse(HttpStatus.INTERNAL_SERVER_ERROR);
+		expect(actual.is2xxOk()).toBe(false);
+		expect(actual.is4xxBadRequest()).toBe(false);
+		expect(actual.is5xxError()).toBe(true);
 	});
 });
--- src/lib/server/apiResponse.ts
+++ src/lib/server/apiResponse.ts
@@ -7,11 +7,13 @@ export class ApiResponse {
 	private readonly requestId: string;
 	private readonly status: Status;
 	private readonly details: object;
+	private readonly httpCode: number;
 
-	constructor(response: { requestId: string; status: string; details: object }) {
+	constructor(response: { requestId: string; status: string; details: object }, httpCode: number) {
 		this.requestId = response.requestId;
 		this.status = response.status === 'SUCCESS' ? Status.SUCCESS : Status.ERROR;
 		this.details = response.details;
+		this.httpCode = httpCode;
 	}
 
 	public getRequestId(): string {
@@ -30,23 +32,41 @@ export class ApiResponse {
 		return !this.isSuccess();
 	}
 
+	public is2xxOk(): boolean {
+		return this.httpCode >= 200 && this.httpCode < 300;
+	}
+
+	public is4xxBadRequest(): boolean {
+		return this.httpCode >= 400 && this.httpCode < 500;
+	}
+
+	public is5xxError(): boolean {
+		return this.httpCode >= 500 && this.httpCode < 600;
+	}
+
 	public getDetails(): object {
 		return this.details;
 	}
 }
 
-export function createFailedApiResponse(details: object): ApiResponse {
-	return new ApiResponse({
-		requestId: '00000000-0000-0000-0000-000000000000',
-		status: 'ERROR',
-		details: details
-	});
+export function createFailedApiResponse(details: object, httpCode: number): ApiResponse {
+	return new ApiResponse(
+		{
+			requestId: '00000000-0000-0000-0000-000000000000',
+			status: 'ERROR',
+			details: details
+		},
+		httpCode
+	);
 }
 
-export function createEmptySuccessApiResponse(): ApiResponse {
-	return new ApiResponse({
-		requestId: '00000000-0000-0000-0000-000000000000',
-		status: 'SUCCESS',
-		details: {}
-	});
+export function createEmptySuccessApiResponse(httpCode: number): ApiResponse {
+	return new ApiResponse(
+		{
+			requestId: '00000000-0000-0000-0000-000000000000',
+			status: 'SUCCESS',
+			details: {}
+		},
+		httpCode
+	);
 }

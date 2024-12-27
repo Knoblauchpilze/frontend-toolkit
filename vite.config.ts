64811a014bcb7bb614e2e4e10345766d70dae6b8
KnoblauchPilze <prolific-programmer@tuta.com>
2024-12-27 13:49:48 +0100

    feat: Setup code coverage report

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
+// https://vitest.dev/config/#coverage-include
+// https://vitest.dev/guide/coverage#coverage-setup
 export default defineConfig({
 	plugins: [sveltekit()],
 
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			reporter: ['json'],
+			include: ['src/lib/**/*.{js,ts,svelte}'],
+			exclude: ['**/index.ts']
+		}
 	}
 });

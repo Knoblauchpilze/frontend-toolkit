fd86f42c9447b83ca07ca8dc51c3db05d3b93156
KnoblauchPilze <prolific-programmer@tuta.com>
2024-12-27 13:15:49 +0100

    feat: Moved server to http and added types to index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+// Components
 export { default as StyledActionButton } from '$lib/components/core/StyledActionButton.svelte';
 export { default as StyledButton } from '$lib/components/core/StyledButton.svelte';
 export { default as StyledError } from '$lib/components/core/StyledError.svelte';
@@ -11,3 +12,12 @@ export { default as Footer } from '$lib/components/layout/Footer.svelte';
 export { default as HeroContainer } from '$lib/components/layout/HeroContainer.svelte';
 
 export { default as FormField } from '$lib/components/FormField.svelte';
+
+// Methods and types
+export { ApiResponse, Status } from '$lib/http/apiResponse.js';
+export { ApiFailure, tryGetFailureReason } from '$lib/http/apiResponseAnalyzer.js';
+export {
+	parseApiResponseAsArray,
+	parseApiResponseAsSingleValue
+} from '$lib/http/apiResponseParser.js';
+export { safeFetchJson, trimTrailingSlash } from '$lib/http/fetch.js';

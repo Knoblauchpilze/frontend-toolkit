7223482e9f941baa98f04b3301ac89280dcf8942
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-29 18:32:28 +0100

    fix: Lint errors

--- src/lib/components/core/StyledTitle.svelte
+++ src/lib/components/core/StyledTitle.svelte
@@ -6,4 +6,4 @@
 	let { text = 'Home' }: Props = $props();
 </script>
 
-<h1 class="px-12 py-4 text-3xl font-bold italic text-secondary">{text}</h1>
+<h1 class="text-secondary px-12 py-4 text-3xl font-bold italic">{text}</h1>

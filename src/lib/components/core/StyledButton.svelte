4fd7305642949ae3fc8e1e1e2f00c98bde13c7a6
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-01-31 14:04:32 +0100

    feat: Buttons' border can be configured

--- src/lib/components/core/StyledButton.svelte
+++ src/lib/components/core/StyledButton.svelte
@@ -2,18 +2,21 @@
 	interface Props {
 		text?: string;
 		enabled?: boolean;
+		border?: boolean;
 	}
 
-	let { text = 'Click me', enabled = true }: Props = $props();
+	let { text = 'Click me', enabled = true, border = false }: Props = $props();
+
+	let borderStyle = $state('');
+	if (border === true) {
+		borderStyle = 'border-solid border-2 border-secondary ';
+	}
+	borderStyle += 'rounded-[8px] px-12 py-4';
 </script>
 
 {#if enabled}
-	<button class="bg-primary text-secondary hover:bg-primary-hover rounded-[8px] px-12 py-4"
-		>{text}</button
-	>
+	<button class="bg-primary text-secondary hover:bg-primary-hover {borderStyle}">{text}</button>
 {:else}
 	<!-- https://stackoverflow.com/questions/34684467/dont-redirect-on-button-click-with-css -->
-	<button disabled class="bg-primary-selected text-secondary rounded-[8px] px-12 py-4"
-		>{text}</button
-	>
+	<button disabled class="bg-primary-selected text-secondary {borderStyle}">{text}</button>
 {/if}

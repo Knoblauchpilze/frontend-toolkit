fe41a2b08e40c9763530d8c4345c89f8387acd78
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-03-03 21:11:45 +0100

    fix: Do not use postcsss to configure tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitest.dev/config/#coverage-include
 // https://vitest.dev/guide/coverage#coverage-setup
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],

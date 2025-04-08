4e0afa4b01b60cc0ba996c9e25f9b614c87fd5eb
KnoblauchPilze <prolific-programmer@tuta.com>
2025-04-08 19:54:58 +0200

    feat: Added styling attribute for StyledButton

--- src/lib/components/core/StyledButton.svelte
+++ src/lib/components/core/StyledButton.svelte
@@ -3,10 +3,17 @@
 		text?: string;
 		enabled?: boolean;
 		border?: boolean;
+		styling?: string;
 		onClick?: () => void;
 	}
 
-	let { text = 'Click me', enabled = true, border = false, onClick }: Props = $props();
+	let {
+		text = 'Click me',
+		enabled = true,
+		border = false,
+		styling = '',
+		onClick
+	}: Props = $props();
 
 	let borderStyle = $state('');
 	if (border === true) {
@@ -16,12 +23,15 @@
 </script>
 
 {#if enabled}
-	<button class="bg-primary text-secondary hover:bg-primary-hover {borderStyle}" onclick={onClick}
-		>{text}</button
+	<button
+		class="bg-primary text-secondary hover:bg-primary-hover {borderStyle} {styling}"
+		onclick={onClick}>{text}</button
 	>
 {:else}
 	<!-- https://stackoverflow.com/questions/34684467/dont-redirect-on-button-click-with-css -->
-	<button disabled class="bg-primary-selected text-secondary {borderStyle}" onclick={onClick}
-		>{text}</button
+	<button
+		disabled
+		class="bg-primary-selected text-secondary {borderStyle} {styling}"
+		onclick={onClick}>{text}</button
 	>
 {/if}

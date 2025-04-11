724231543df60bb2379be9bca52162e5ad07fac0
KnoblauchPilze <maxime.rigaud.perso@gmail.com>
2025-04-11 18:20:14 +0200

    feat: Allow StyledTitle to specify the text size

--- src/lib/components/core/StyledTitle.svelte
+++ src/lib/components/core/StyledTitle.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	interface Props {
 		text?: string;
+		textSize?: string;
 		margins?: boolean;
 	}
 
-	let { text = 'Home', margins = true }: Props = $props();
+	let { text = 'Home', textSize = 'text-3xl', margins = true }: Props = $props();
 
 	let marginsStyle = $state('');
 	if (margins === true) {
@@ -12,4 +13,4 @@
 	}
 </script>
 
-<h1 class="text-secondary text-3xl font-bold italic {marginsStyle}">{text}</h1>
+<h1 class="text-secondary font-bold italic {textSize} {marginsStyle}">{text}</h1>
